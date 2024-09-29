4221d805ef353c5a3ff4b611e5119f678f57ce7b
Daniel Riyanto <danielriyanto190700@gmail.com>
2024-09-29 11:25:26 +0700

    (feat) Setup auth routes in backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes";
+
 dotenv.config();
 
 const app: Express = express();
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
